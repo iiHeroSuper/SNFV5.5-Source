b00228dc2bfe0cd4d8a194368b7eb10a9f9248e3
Herox/Arie Temps <77843358+iiHeroSuper@users.noreply.github.com>
2025-09-22 09:37:09 -0300

    Update app icon and window name.lua

--- assets/shared/scripts/app icon and window name.lua
+++ assets/shared/scripts/app icon and window name.lua
@@ -2,24 +2,24 @@
 
 
 function onCreate()
-    addLuaScript('loadscripts/notefadecontrol')
-    
-
-    -- Icon in the image folder, do not write .png, just the name. Also I only tested it with 200x200 so not sure if it works with another image resolution
-
-    local iconName = "icon16X"
-    
-    addHaxeLibrary("Application", "lime.app")
-    addHaxeLibrary("Image", "lime.graphics")
-    
-    local haxeCode = string.format([[
-        var icon:Image = Image.fromFile(Paths.modFolders("images/%s.png"));
-        Application.current.window.setIcon(icon);
-    ]], iconName)
-    runHaxeCode(haxeCode)
+    --addLuaScript('loadscripts/notefadecontrol')
+    --
+	--
+    ---- Icon in the image folder, do not write .png, just the name. Also I only tested it with 200x200 so not sure if it works with another image resolution
+	--
+    --local iconName = "icon16X"
+    --
+    --addHaxeLibrary("Application", "lime.app")
+    --addHaxeLibrary("Image", "lime.graphics")
+    --
+    --local haxeCode = string.format([[
+    --    var icon:Image = Image.fromFile(Paths.modFolders("images/%s.png"));
+    --    Application.current.window.setIcon(icon);
+    --]], iconName)
+    --runHaxeCode(haxeCode)
     
 
     -- Set the window title write whatever you want
 
-    setPropertyFromClass("openfl.Lib", "application.window.title", "Sonic Night Funkin' V5.0")
+    --setPropertyFromClass("openfl.Lib", "application.window.title", "Sonic Night Funkin' V5.0")
 end
