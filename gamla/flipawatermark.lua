bbba114dfec477a06677e56b241858c776518e73
Herox/Arie Temps <77843358+iiHeroSuper@users.noreply.github.com>
2025-09-22 09:13:57 -0300

    Delete gamla directory (error)

--- gamla/flipawatermark.lua
+++
@@ -1,7 +0,0 @@
--- made by Terionic haha boom pooy
-function onCreate()
-	makeLuaSprite("watermark","BFS1GamlaWatermark",0,0);
-	setScrollFactor('watermark',0,0);
-	setObjectCamera('watermark','other');
-	addLuaSprite("watermark",true);
-end
