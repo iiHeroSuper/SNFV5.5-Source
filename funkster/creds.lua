80c33b491815b485ae08745cf013d29eed1b1066
Herox/Arie Temps <77843358+iiHeroSuper@users.noreply.github.com>
2025-09-22 09:13:20 -0300

    Delete funkster directory (error)

--- funkster/creds.lua
+++
@@ -1,9 +0,0 @@
-local allowCountdown = false
-function onEndSong()
-    if isStoryMode and not seenCutscene then
-        startVideo('credits')
-        seenCutscene = true
-        return Function_Stop
-    end
-    return Function_Continue
-end
